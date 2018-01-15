efa574c8163f25bdae7da9868af5e33829024ff0
Joaquín Solano <33550023+JoaquinSolanoA@users.noreply.github.com>
2018-01-14 21:57:39 -0300

    Agregar Context en el archivo StartUp

--- Cotizaciones/Startup.cs
+++ Cotizaciones/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using Cotizaciones.Data;
 namespace Cotizaciones
 {
     public class Startup
@@ -22,6 +22,7 @@ namespace Cotizaciones
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<CotizacionesContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
