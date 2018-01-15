313437d5c3bcaf955df041670f2235b929515037
Joaquín Solano <33550023+JoaquinSolanoA@users.noreply.github.com>
2018-01-15 00:18:45 -0300

    Creacion de los Controladores de las clases

---
+++ Cotizaciones/Controllers/CotizacionController.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Cotizaciones.Data;
+using Cotizaciones.Models;
+
+namespace Cotizaciones.Controllers
+{
+    public class CotizacionController : Controller
+    {
+        private readonly CotizacionesContext _context;
+
+        public CotizacionController(CotizacionesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cotizacion
+        public async Task<IActionResult> Index()
+        {
+            var cotizacionesContext = _context.Cotizaciones.Include(c => c.Cliente);
+            return View(await cotizacionesContext.ToListAsync());
+        }
+
+        // GET: Cotizacion/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cotizacion = await _context.Cotizaciones
+                .Include(c => c.Cliente)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (cotizacion == null)
+            {
+                return NotFound();
+            }
+
+            return View(cotizacion);
+        }
+
+        // GET: Cotizacion/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id");
+            return View();
+        }
+
+        // POST: Cotizacion/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ClienteId,NReferencia,TotalNeto,Impuesto,FechaEmision,FechaVencimiento")] Cotizacion cotizacion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cotizacion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id", cotizacion.ClienteId);
+            return View(cotizacion);
+        }
+
+        // GET: Cotizacion/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cotizacion = await _context.Cotizaciones.SingleOrDefaultAsync(m => m.Id == id);
+            if (cotizacion == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id", cotizacion.ClienteId);
+            return View(cotizacion);
+        }
+
+        // POST: Cotizacion/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ClienteId,NReferencia,TotalNeto,Impuesto,FechaEmision,FechaVencimiento")] Cotizacion cotizacion)
+        {
+            if (id != cotizacion.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cotizacion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CotizacionExists(cotizacion.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id", cotizacion.ClienteId);
+            return View(cotizacion);
+        }
+
+        // GET: Cotizacion/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cotizacion = await _context.Cotizaciones
+                .Include(c => c.Cliente)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (cotizacion == null)
+            {
+                return NotFound();
+            }
+
+            return View(cotizacion);
+        }
+
+        // POST: Cotizacion/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cotizacion = await _context.Cotizaciones.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Cotizaciones.Remove(cotizacion);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CotizacionExists(int id)
+        {
+            return _context.Cotizaciones.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Cotizaciones/Controllers/ServicioController.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Cotizaciones.Data;
+using Cotizaciones.Models;
+
+namespace Cotizaciones.Controllers
+{
+    public class ServicioController : Controller
+    {
+        private readonly CotizacionesContext _context;
+
+        public ServicioController(CotizacionesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Servicio
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Servicios.ToListAsync());
+        }
+
+        // GET: Servicio/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var servicio = await _context.Servicios
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+
+            return View(servicio);
+        }
+
+        // GET: Servicio/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Servicio/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Descripcion,Cantidad,ValorUnitario,TotalValor")] Servicio servicio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(servicio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servicio);
+        }
+
+        // GET: Servicio/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var servicio = await _context.Servicios.SingleOrDefaultAsync(m => m.Id == id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+            return View(servicio);
+        }
+
+        // POST: Servicio/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Descripcion,Cantidad,ValorUnitario,TotalValor")] Servicio servicio)
+        {
+            if (id != servicio.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(servicio);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServicioExists(servicio.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servicio);
+        }
+
+        // GET: Servicio/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var servicio = await _context.Servicios
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+
+            return View(servicio);
+        }
+
+        // POST: Servicio/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var servicio = await _context.Servicios.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Servicios.Remove(servicio);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServicioExists(int id)
+        {
+            return _context.Servicios.Any(e => e.Id == id);
+        }
+    }
+}
