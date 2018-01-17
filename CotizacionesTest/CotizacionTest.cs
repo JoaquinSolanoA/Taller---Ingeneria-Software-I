bd1bda9d584a2e549b6615ac43c1715129b51d83
Joaquín Solano <33550023+JoaquinSolanoA@users.noreply.github.com>
2018-01-17 18:36:28 -0300

    Se creó el testing
    Sin metodos por el momento el testing.

---
+++ CotizacionesTest/CotizacionTest.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using Cotizaciones.Models; // Aun no se decide el lugar de los metodos a comprobar.
+/// <summary>
+/// Archivo que se esta usando para las pruebas testing.
+/// </summary>
+namespace CotizacionesTes {
+
+    /// <summary>
+    /// Clase que realiza distintaas pruebas para la verificaciones de metodos.
+    /// </summary>
+    public class CotizacionTest {
+
+        /// <summary>
+        /// Metodo Test que hace las comprobaciones.
+        /// </summary>
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}
